b88844e6868d69f9b12af97916da647d05594a91
Aina Wangsmo <wangsmo_aina@hotmail.com>
2020-10-22 13:10:52 +0200

    Startet på oppgave 1, laget "skallet" ved hjelp av programkode 5.2.3a i kompendiet

--- src/no/oslomet/cs/algdat/Eksamen/EksamenSBinTre.java
+++ src/no/oslomet/cs/algdat/Eksamen/EksamenSBinTre.java
@@ -1,12 +1,12 @@
 package no.oslomet.cs.algdat.Eksamen;
 
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.StringJoiner;
+import java.sql.SQLOutput;
+import java.util.*;
 
 public class EksamenSBinTre<T> {
+
+
     private static final class Node<T>   // en indre nodeklasse
     {
         private T verdi;                   // nodens verdi
@@ -33,6 +33,8 @@ public class EksamenSBinTre<T> {
 
     } // class Node
 
+
+
     private Node<T> rot;                            // peker til rotnoden
     private int antall;                             // antall noder
     private int endringer;                          // antall endringer
@@ -44,6 +46,63 @@ public class EksamenSBinTre<T> {
         rot = null;
         antall = 0;
         comp = c;
+    }
+
+
+
+
+
+    public String toStringPostOrder() {
+        if (tom()) return "[]";
+
+        StringJoiner s = new StringJoiner(", ", "[", "]");
+
+        Node<T> p = førstePostorden(rot); // går til den første i postorden
+        while (p != null) {
+            s.add(p.verdi.toString());
+            p = nestePostorden(p);
+        }
+
+        return s.toString();
+    }
+
+
+    /////////////////////////// OPPGAVE 1 //////////////////////////////////////////////
+
+    /*  Kopiert kode fra kompendiet 5.2.3a) */
+    public boolean leggInn(T verdi) {
+        Objects.requireNonNull(verdi, "Ikke love med null-verdier");
+
+        Node<T> p = rot, forelder = null;  //p starter i roten
+        int cmp = 0;    //hjelpevariabel
+
+        while(p != null) {  //fortsetter til p er ute av treet
+            forelder = p;  //forelder er forelder til p
+            cmp = comp.compare(verdi, p.verdi); //bruker komparatoren
+            if(cmp < 0) {
+                forelder = p.venstre;
+            }
+            else {
+                forelder = p.høyre;
+            }
+        }
+
+        //p er nå null, som betyr at den er ute av treet, forelder er den siste vi passerte
+
+        p = new Node<>(verdi);  //oppretter en ny node
+
+        if(forelder == null) rot = p;  //p blir rotnode
+        else if(cmp < 0) forelder.venstre = p; //venstre barn til q
+        else forelder.høyre = p;   //høyre barn til q
+
+        antall++;   //en verdi mer i treet
+        return true;    //vellykket innlegging
+
+    }
+
+    /////////////////////////// OPPGAVE 2 //////////////////////////////////////////////
+    public boolean tom() {
+        return antall == 0;
     }
 
     public boolean inneholder(T verdi) {
@@ -65,30 +124,6 @@ public class EksamenSBinTre<T> {
         return antall;
     }
 
-    public String toStringPostOrder() {
-        if (tom()) return "[]";
-
-        StringJoiner s = new StringJoiner(", ", "[", "]");
-
-        Node<T> p = førstePostorden(rot); // går til den første i postorden
-        while (p != null) {
-            s.add(p.verdi.toString());
-            p = nestePostorden(p);
-        }
-
-        return s.toString();
-    }
-    public boolean tom() {
-        return antall == 0;
-    }
-
-    /////////////////////////// OPPGAVE 1 //////////////////////////////////////////////
-
-    public boolean leggInn(T verdi) {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
-    }
-
-    /////////////////////////// OPPGAVE 2 //////////////////////////////////////////////
 
     public int antall(T verdi) {
         throw new UnsupportedOperationException("Ikke kodet ennå!");
